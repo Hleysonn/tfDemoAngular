ad1452d4d3e3c1009682fc391a3df63dfba446a4
Khun <khun.ly@bstorm.be>
2024-03-19 10:17:26 +0100

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  produits: any[] = [
-    { titre: 'Coca cola', contenu: 'Boisson sucrée pas bonne pour la santé', image: 'https://m.media-amazon.com/images/I/5156FefjlqL.jpg' },
-    { titre: 'Fanta orange', contenu: 'Boisson au goût orange', image: 'https://www.coca-cola.com/content/dam/onexp/be/fr/brands/fanta/REN2308-00183_BIC_FO_CANSLK_330ml_BEL%20_%20LUX_FR_D_Closed_5000112638783_724.png' },
-    { titre: 'Dr pepper', contenu: 'Boisson au goût ???', image: 'https://mesbonbons.net/871-large_default/dr-pepper-23-flavour-33-cl.jpg' }
-  ]
-
-  constructor() {
-  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { MessageComponent } from './components/message/message.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MenuComponent } from './layout/menu/menu.component';
+import { Demo1Component } from './pages/demo1/demo1.component';
+import { Demo2Component } from './pages/demo2/demo2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     MessageComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    Demo1Component,
+    Demo2Component,
   ],
   imports: [
     BrowserModule,
